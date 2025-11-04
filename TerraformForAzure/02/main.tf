1067378abe40298f4ca688cf818d941d0455c32c
Koortas <58269894+Koortas@users.noreply.github.com>
2025-11-04 08:44:14 -0500

    maj

--- TerraformForAzure/02/main.tf
+++ TerraformForAzure/02/main.tf
@@ -31,9 +31,9 @@ resource "azurerm_virtual_network" "main" {
   name = "vnet-az1-testterraform"
   address_space = ["10.0.0.0/24"]
   location = "Canada Central"
-  ressource_group_name = azurerm_resource_group.main.name
+  resource_group_name = azurerm_resource_group.main.name
   tags = {
-    environnement = "lab"
+        environnement = "lab"
         produit      = "TerraformForAzure"
         realisateur  = "jfgauthier"
         proprietaire   = "DPII"
